1c3c9430a172199ae126691348c21100f9cb09cf
Fatcat0712 <maisonhai456@gmail.com>
2026-03-19 09:11:31 +0700

    initialize backend

---
+++ hungry-belly-backend/src/main/java/com/eddie/hungry_belly_backend/HungryBellyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eddie.hungry_belly_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HungryBellyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HungryBellyBackendApplication.class, args);
+	}
+
+}
---
+++ hungry-belly-backend/src/test/java/com/eddie/hungry_belly_backend/HungryBellyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eddie.hungry_belly_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HungryBellyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
